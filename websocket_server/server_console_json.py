fe5b2b46b1d1e6fbfa82555f0a3eccffae042123
Patrick Borgeat <patrick@borgeat.de>
2020-09-21 11:22:23 +0300

    use a code snippet for an async input; hopefully improving the server consoles

--- websocket_server/server_console_json.py
+++ websocket_server/server_console_json.py
@@ -2,8 +2,18 @@ import asyncio
 import websockets
 import json
 import argparse
+import sys
 
+from concurrent.futures import ThreadPoolExecutor
 from netstuff import *
+
+# https://gist.github.com/delivrance/675a4295ce7dc70f0ce0b164fcdbd798
+async def ainput(prompt: str = ""):
+    with ThreadPoolExecutor(1, "AsyncInput", lambda x: print(x, end="", flush=True), (prompt,)) as executor:
+        return (await asyncio.get_event_loop().run_in_executor(
+            executor, sys.stdin.readline
+        )).rstrip()
+
 
 parser = argparse.ArgumentParser(description='JSON command console for CodeklaviAR websocket server')
 
@@ -23,7 +33,7 @@ else:
 async def repl():
     async with websockets.connect(ws_uri, ping_interval=3, ping_timeout=None) as websocket:
     	while True: 
-        	cmd = input("Enter CKAR JSON Command: ")
+        	cmd = await ainput("Enter CKAR JSON Command: ")
         	await websocket.send(cmd)
 
 asyncio.get_event_loop().run_until_complete(repl())
--- websocket_server/server_console_lsys.py
+++ websocket_server/server_console_lsys.py
@@ -3,8 +3,18 @@ import websockets
 import json
 import argparse
 
+import sys
 
+from concurrent.futures import ThreadPoolExecutor
 from netstuff import *
+
+# https://gist.github.com/delivrance/675a4295ce7dc70f0ce0b164fcdbd798
+async def ainput(prompt: str = ""):
+    with ThreadPoolExecutor(1, "AsyncInput", lambda x: print(x, end="", flush=True), (prompt,)) as executor:
+        return (await asyncio.get_event_loop().run_in_executor(
+            executor, sys.stdin.readline
+        )).rstrip()
+
 
 parser = argparse.ArgumentParser(description='LSystem command console for CodeklaviAR websocket server')
 
@@ -25,7 +35,7 @@ else:
 async def repl():
     async with websockets.connect(ws_uri, ping_interval=3, ping_timeout=None) as websocket:
     	while True: 
-        	cmd = input("Enter CKAR LSystem Command: ")
+        	cmd = await ainput("Enter CKAR LSystem Command: ")
         	await websocket.send(json.dumps({"type": "lsys", "payload": cmd}))
 
 asyncio.get_event_loop().run_until_complete(repl())
