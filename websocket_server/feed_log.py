85703fdaa09c252f81086eec32dfe5c74446e70e
Patrick Borgeat <patrick@borgeat.de>
2021-01-07 18:34:49 +0100

    added new options to the log feeder for use in demo server

--- websocket_server/feed_log.py
+++ websocket_server/feed_log.py
@@ -6,12 +6,24 @@ import datetime
 
 from netstuff import *
 
-parser = argparse.ArgumentParser(description='JSON command console for CodeklaviAR websocket server')
+parser = argparse.ArgumentParser(description='Log replay for CodeklaviAR websocket server')
 
 parser.add_argument('-l', '--local',
-	help="connect to local websocket server.",
+	help="connect to local websocket server",
 	dest="local",
 	action="store_true"
+)
+
+parser.add_argument('-e', '--endless',
+	help="play back log in a loop",
+	dest="loop",
+	action="store_true"
+)
+
+parser.add_argument('-u', '--url',
+	help="specify websocket serve URI",
+	dest="uri",
+	default=""
 )
 
 parser.add_argument("-f", "--file",
@@ -30,12 +42,16 @@ parser.add_argument("-t", "--timestretch",
 
 args = vars(parser.parse_args())
 
-if args["local"]:
+if args["uri"] != "":
+	ws_uri = args["uri"]
+elif args["local"]:
 	ws_uri = get_local_websocket_uri("ckar_serve")
 else:
 	ws_uri = get_websocket_uri("ckar_serve")
 
 ts = float(args["timestretch"])
+
+doLoop = args["loop"]
 
 msgs =  []
 with open(args["file"]) as fp:
@@ -57,10 +73,14 @@ with open(args["file"]) as fp:
 
 
 async def feed():
-    async with websockets.connect(ws_uri, ping_interval=3, ping_timeout=None) as websocket:
-    	for msg in msgs:
-    		print(msg[1])
-    		await websocket.send(msg[1])
-    		await asyncio.sleep(msg[0] * ts)
+	async with websockets.connect(ws_uri, ping_interval=3, ping_timeout=None) as websocket:
+		continueLooping = True
+		while continueLooping:
+			for msg in msgs:
+				print(msg[1])
+				await websocket.send(msg[1])
+				await asyncio.sleep(msg[0] * ts)
+			if not doLoop:
+				continueLooping = False
 
 asyncio.get_event_loop().run_until_complete(feed())
