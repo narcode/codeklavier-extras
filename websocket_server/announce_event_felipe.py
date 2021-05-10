d907a5158e1c5c5cb888d792d6d7a8c224ee2112
narcode <contact@felipeignacio.info>
2021-05-10 11:53:07 +0200

    synch

---
+++ websocket_server/announce_event_felipe.py
@@ -0,0 +1,27 @@
+from netstuff import *
+import json
+
+# to select which channel the information is sent to put
+# put the according master-credentials.json file into
+# the folder.
+
+# set eventISODate and eventURL to "" to 'unannounce'
+# one could also set "name" to change the channel name
+
+set_channel_status(json.dumps({
+	"description": "Anne is aging pooily chocolate trouble ahead",
+	"eventISODate": "",
+	"eventURL": "",
+	"name": "Rehearsals",
+	"visible": True,
+	"status": "online"
+}));
+
+# relevant info keys:
+# - status ("online" or "offline")
+# - name
+# - description
+# - websocketBaseURL (don't set manually ...)
+# - eventURL
+# - eventISODate (like: 2021-04-16T18:00:00Z)
+# - --> note: should be in UTC timezone
