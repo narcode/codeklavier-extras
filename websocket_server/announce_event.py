e4c26500ed58b193077f4169c1f60e06c3a03726
cappelnord <patrick@borgeat.de>
2021-05-22 14:10:56 +0200

    clarified event announcements; added receive-only mode for relay (for testing) fixed counting of sent messages

--- websocket_server/announce_event.py
+++ websocket_server/announce_event.py
@@ -5,21 +5,21 @@ import json
 # put the according master-credentials.json file into
 # the folder.
 
-# set eventISODate and eventURL to "" to 'unannounce'
-# one could also set "name" to change the channel name
-
 set_channel_status(json.dumps({
 	"description": "This is channel is usually hidden and top secret, so I wonder if your name might actually be Anne or Felipe?"
 }));
 
 # relevant info keys:
-# - status ("online" or "offline")
-# - name
-# - description
+# - status (string, "online" or "offline")
+# - name (string)
+# - description (string)
 # - websocketBaseURL (don't set manually ...)
-# - eventURL
-# - visible (true or false as boolean, not as string!!)
+# - eventURL (string)
+# - visible (True or False as boolean, not as string!!)
 # - baseScale (number, 1.0 on default)
 # - baseDistance (number, 1.0 on default)
 # - eventISODate (like: 2021-04-16T18:00:00Z)
 # - --> note: should be in UTC timezone
+
+# set eventISODate and eventURL to "" to 'unannounce'
+# one could also set "name" to change the channel name
